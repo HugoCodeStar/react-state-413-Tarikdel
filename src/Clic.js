47cff0f45e118eb2f145e4b4a00dc4df5c802e04
Admin <tarik@gmail.com>
2021-01-17 11:39:30 -0500

    version 2 d'exo react-state

--- src/Clic.js
+++ src/Clic.js
@@ -2,13 +2,13 @@ import {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 
 function Clic(){
-    let cpt=0;
-    const [compteur, setCompteur] = useState(cpt);
+
+    const [compteur, setCompteur] = useState(0);
 
     return (
         <div>
-            The count is {compteur}
-            <Button onClick={() => setCompteur(compteur+1)}>Click</Button>
+            The count is {compteur}{' '}
+            <Button variant="primary" onClick={() => setCompteur(compteur+1)}>Click</Button>{' '}
             <Button onClick={() => setCompteur(0)}>Reset</Button>
 
         </div>
